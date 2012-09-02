598150d632e913760cb933566c04519960c0b219
Michael Watson <the.mwatson@gmail.com>
2012-09-02 00:49:14 -0700

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saber::Application.config.secret_token = '97b5b1bef26c17c5de785b56036683d803d2946832adb4862100ccbec8779631899f899f62a3ab7933682f9124dcb170fe36e2b7459155ba5a35d40211cc2966'
